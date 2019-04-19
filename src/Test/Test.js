7d8aca22b6edae927029430afaad77559bbe3577
rs-shellington <scott.shellington@gmail.com>
2019-04-19 13:54:32 -0400

    wip

---
+++ src/Test/Test.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './Test.css'
+
+class Test extends Component {
+
+    render() {
+        return(
+            <div>
+            <p>Test</p>
+            <p>Right!</p>
+            <table>
+                <tbody>
+                <tr>
+                    <td>one</td>
+                    <td>two</td>
+                </tr>
+                <tr>
+                    <td>three</td>
+                    <td>four</td>
+                </tr>
+            </tbody>
+            </table>
+            
+            </div>
+        );  
+    }
+
+}
+
+export default Test;
