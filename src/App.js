e706adec50503690f7b8a7174d57a966169e57ed
rs-shellington <scott.shellington@gmail.com>
2019-04-19 12:57:46 -0400

    change to observe build process

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ class App extends Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+
+          <p>-RSS-</p>
           <a
             className="App-link"
             href="https://reactjs.org"
