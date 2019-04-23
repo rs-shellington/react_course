d851fdc0bff0b91dee0cecc11a9b043d3cd85602
rs-shellington <scott.shellington@gmail.com>
2019-04-23 14:31:07 -0400

    wip

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,10 @@
 
 import React from 'react';
-
+import './Person.css'
 
 const person = (props) => {
     return( 
-        <div>
+        <div className = 'Person'>
             <p>I'm {props.name} and I am {props.age}</p>
             <p>{props.children}</p>  
             <input type='text' onChange={props.change} value={props.name}/>  
