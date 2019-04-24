8a129346fef050630d009b9a69e0dd446363f1dd
rs-shellington <scott.shellington@gmail.com>
2019-04-24 10:22:40 -0400

    wip

--- src/Dumb/Dumb.js
+++ src/Dumb/Dumb.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const dumb = () => {return <h3>Dumb component!</h3>}
+const dumb = () => {
+    
+    return (
+     <h3>Dumb component!</h3>
+    )
+    
+}
 
 export default dumb;
