e7f7b8b41e537778578078adb8973edfabecd1ae
rs-shellington <scott.shellington@gmail.com>
2019-04-24 17:16:48 -0400

    wip

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
 import Test from './Test/Test'
@@ -12,18 +12,22 @@ class App extends Component {
     persons: [{ name: 'Scott', age: 57 },
     { name: 'Kylie', age: 26 },
     { name: 'Kathy', age: 59 }
-    ]
+    ],
+    showPersons: false,
   }
 
   switchNameHandler = () => {
 
     console.log('click')
     // Wrong: this.state.persons[2].age = '[Redacted]';
+    let toggle = !this.state.showPersons;
+
     this.setState(
       {
         persons: [{ name: 'Scott', age: 57 },
         { name: 'Kylie', age: 26 },
-        { name: 'Kathy', age: '[Redacted]' }]
+        { name: 'Kathy', age: '[Redacted]' }],
+        showPersons: toggle,
       })
 
   };
@@ -49,20 +53,11 @@ class App extends Component {
 
     }
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+    let persons = null;
 
-          <p>-RSS-</p>
-          <Test />
-          <Dumb />
-          <button 
-          style={style}
-          onClick={this.switchNameHandler}>Switch Name</button>
+    if(this.state.showPersons) {
+      persons = (
+        <div>
           <Person 
              name={this.state.persons[0].name} 
              age={this.state.persons[0].age}>Hobbies: (Bad) Golf
@@ -79,6 +74,22 @@ class App extends Component {
              age={this.state.persons[2].age} 
 
           />
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          
+
+          <p>-RSS-</p>
+          <Test />
+          <Dumb />
+          <button 
+          style={style}
+          onClick={this.switchNameHandler}>Switch Name</button>
+          {persons}
           <a
             className="App-link"
             href="https://reactjs.org"
--- src/Test/Test.js
+++ src/Test/Test.js
@@ -8,18 +8,7 @@ class Test extends Component {
             <div>
             <p>Test</p>
             <p>Right!</p>
-            <table>
-                <tbody>
-                <tr>
-                    <td>one</td>
-                    <td>two</td>
-                </tr>
-                <tr>
-                    <td>three</td>
-                    <td>four</td>
-                </tr>
-            </tbody>
-            </table>
+            
             
             </div>
         );  
